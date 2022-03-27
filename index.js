d1da28d56fbbd9f0b58dfd833f8056cb3c0d2ea4
Edgar <franky9202@gmail.com>
2022-03-27 11:07:26 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const cards = []
+function writeCards(names, eventname){
+    for (let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return cards
+}
+writeCards(cards)
+
+function countDown(){
+    let callcount = 10
+    while (callcount > -1) {
+        console.log(callcount --)
+    }
+    return countDown
+}
